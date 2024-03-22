ccd27d1c0851815ea6df0762fcfd35270210edda
Michel Barakat <michel.b.barakat@gmail.com>
2024-03-22 17:34:19 +0100

    cleanup and some general SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,32 @@
 import { Metadata, Viewport } from "next";
 import { PropsWithChildren } from "react";
 
-// import { headers } from 'next/headers';
-import './globals.css';
+import "./globals.css";
 
 export const generateMetadata = async (): Promise<Metadata> => {
-  //   const host = headers().get('host');
-  //   const baseURL = `${protocol}://${host ?? vercelURL}`;
-  const siteName = `Professional Résumé`;
-  const title = `Résumé`;
-  const description = `Professional résumé .`;
+  const siteName = "Volvo Cars | Swedish luxury automobile marque";
+  const title = "Volvo Cars | Swedish luxury automobile marque";
+  const description =
+    "Volvo Cars is a Swedish luxury automobile marque. It is headquartered in Torslanda in Gothenburg, Sweden. The company manufactures and markets sport utility vehicles, station wagons, sedans and compact executive sedans. The Volvo Group was founded in 1927 as a subsidiary of the ball bearing manufacturer SKF. When AB Volvo bought the car manufacturer in 1999, the sales of the Volvo Group increased significantly. The company was sold to Geely Holding Group in 2010. Volvo Cars has been majority-owned since 2010 by the Geely Holding Group, a Chinese multinational automotive manufacturing company. Both AB Volvo and Volvo Cars share the Volvo logo, and cooperate in running the Volvo Museum in Sweden.";
 
   return {
-    // metadataBase: new URL(baseURL),
     applicationName: siteName,
     description,
     generator: "Next.js",
-    keywords: ["resume", "next.js", "pdf", "CV", "smart resume", "open ai"],
+    keywords: ["Volvo", "Volvo Cars", "Volvo Museum", "Geely Holding Group"],
     title,
   };
 };
 
-// export const viewport: Viewport = {
-//   initialScale: 1,
-//   // @ts-ignore
-//   themeColor: colors[accentColor][`${accentColor}9`],
-//   width: "device-width",
-// };
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: "device-width",
+};
 
 const RootLayout: React.FC<PropsWithChildren> = async ({ children }) => {
   return (
-    <html
-      lang="en"
-      //   className={twMerge(
-      //     albert.variable,
-      //     jetBrainsMono.variable,
-      //     resumeConfig.appTheme === ThemeSetting.Dark && 'dark',
-      //   )}
-    >
-      <body className="overflow-hidden bg-slate-900 text-neutral-12 selection:bg-accent-11 selection:text-neutral-1">
-        {children}
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 };
