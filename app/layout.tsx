e42b9de1e9abcd94ea41e9cac716a7e261c342a5
Michel Barakat <michel.barakat@bertelsmann.de>
2024-03-22 17:15:28 +0100

    folder restructure (modern conventions)

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import { Metadata, Viewport } from "next";
+import { PropsWithChildren } from "react";
+
+// import { headers } from 'next/headers';
+import './globals.css';
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  //   const host = headers().get('host');
+  //   const baseURL = `${protocol}://${host ?? vercelURL}`;
+  const siteName = `Professional Résumé`;
+  const title = `Résumé`;
+  const description = `Professional résumé .`;
+
+  return {
+    // metadataBase: new URL(baseURL),
+    applicationName: siteName,
+    description,
+    generator: "Next.js",
+    keywords: ["resume", "next.js", "pdf", "CV", "smart resume", "open ai"],
+    title,
+  };
+};
+
+// export const viewport: Viewport = {
+//   initialScale: 1,
+//   // @ts-ignore
+//   themeColor: colors[accentColor][`${accentColor}9`],
+//   width: "device-width",
+// };
+
+const RootLayout: React.FC<PropsWithChildren> = async ({ children }) => {
+  return (
+    <html
+      lang="en"
+      //   className={twMerge(
+      //     albert.variable,
+      //     jetBrainsMono.variable,
+      //     resumeConfig.appTheme === ThemeSetting.Dark && 'dark',
+      //   )}
+    >
+      <body className="overflow-hidden bg-slate-900 text-neutral-12 selection:bg-accent-11 selection:text-neutral-1">
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { HelloWorld } from '../components/HelloWorld';
+
+const HomePage = () => {
+    return (
+        <div>
+            <HelloWorld />
+        </div>
+    );
+};
+
+export default HomePage;
